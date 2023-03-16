97ae8ac576606d4f8008ee72a3af2c7369bbc9ff
Kaushal Mehta <Kaushalkumar304@gmail.com>
2023-03-16 14:18:46 +0530

    hiding input password field

--- src/app/components/my-input/my-input.component.ts
+++ src/app/components/my-input/my-input.component.ts
@@ -3,14 +3,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-my-input',
   templateUrl: './my-input.component.html',
-  styleUrls: ['./my-input.component.css']
+  styleUrls: ['./my-input.component.css'],
 })
 export class MyInputComponent {
   @Output() inputValue = new EventEmitter<string>();
-  @Input() name: string = "";
+  @Input() name: string = '';
+  @Input() type: string = 'text';
 
   onInputChange(event: any) {
-    this.inputValue.emit(JSON.stringify({ value: event.target.value, name: event.target.name}));
+    this.inputValue.emit(
+      JSON.stringify({ value: event.target.value, name: event.target.name })
+    );
   }
-
 }
